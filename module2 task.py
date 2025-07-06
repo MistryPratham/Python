9d92e9d05afbb6590bc7ddcea0cc58a3800f563b
MistryPratham <mistrypratham2006@gmail.com>
2025-07-06 14:40:28 +0530

    Add files via upload

---
+++ module2 task.py
@@ -0,0 +1,17 @@
+
+# TASK 1 Perform Basic Mathematical Operations
+
+a = int(input('Enter the first number: '))
+b = int(input('Enter the second number: '))
+print("Addition", a+b)
+print("Subtraction", a-b)
+print("Multiplication", a*b)
+print("Division", a/b)
+
+
+# Task 2: Create a Personalized Greeting
+
+
+firstname = input('Enter your first name: ')
+lastname = input('Enter your last name: ')
+print("Hello,", firstname+ lastname,"! Welcome to Python program.")
